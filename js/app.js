5d24c1c3996b86e2dacc8da0236b0a45f07cd61b
Jeremy Underwood <underwood.j248@gmail.com>
2022-02-04 23:06:38 -0500

    Start init function

--- js/app.js
+++ js/app.js
@@ -1,18 +1,27 @@
 /*-------------------------------- Constants --------------------------------*/
-const boardArr = [], turn, winner
+
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
+let gameStatus, boardArr = [], turn, winner
 
 
 /*------------------------ Cached Element References ------------------------*/
-
-
+const divs = document.querySelectorAll('div')
+const messageEl = document.querySelector('#message')
+//Store the element that displays the game status on the page
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 
 
 /*-------------------------------- Functions --------------------------------*/
+init()
 
+function init() {
+    boardArr.forEach(function(elmnt) {
+        elmnt.push(1)
+        //console.log(elmnt)
+    })
+    console.log(boardArr)
+}
